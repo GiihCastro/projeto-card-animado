b6e828eefcca1151802812435220445cad42e27e
Giovanna <160510344+GiihCastro@users.noreply.github.com>
2024-07-02 10:51:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,43 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
     var zoomImage = document.getElementById('zoomImage');
     var eyeIcon = document.getElementById('eye');
     var eyeOffIcon = document.getElementById('eyeOff');
-    var cardImage = document.querySelector('.card-image');
     var cardContent = document.getElementById('cardContent');
+    
+    eyeOffIcon.style.display = 'none';
+
+    zoomImage.addEventListener('mouseenter', function() {
+        this.style.transform = 'scale(1.2)';
+    });
+
+    zoomImage.addEventListener('mouseleave', function() {
+        this.style.transform = 'scale(1)';
+    });
 
     eyeIcon.addEventListener('click', function() {
-        if (cardContent.classList.contains('show')) {
-            cardContent.classList.remove('show');
-            setTimeout(function() {
-                cardImage.style.display = 'block';
-                cardContent.classList.add('hide');
-                eyeOffIcon.style.display = 'block';
-            }, 300); // Tempo da animação em milissegundos
-        } else {
-            cardImage.style.display = 'none';
-            cardContent.classList.remove('hide');
-            eyeOffIcon.style.display = 'block';
-            setTimeout(function() {
-                cardContent.classList.add('show');
-            }, 50); // Pequeno atraso para garantir que a animação funcione corretamente
-        }
-        toggleEyeIcon();
+        zoomImage.style.display = 'none';
+        cardContent.style.display = 'block';
+        eyeIcon.style.display = 'none';
+        eyeOffIcon.style.display = 'block';
     });
 
     eyeOffIcon.addEventListener('click', function() {
-        cardContent.classList.remove('show');
-        setTimeout(function() {
-            cardContent.classList.add('hide');
-            cardImage.style.display = 'block';
-            eyeOffIcon.style.display = 'none';
-        }, 300); // Tempo da animação em milissegundos
-        toggleEyeIcon();
+        zoomImage.style.display = 'block';
+        cardContent.style.display = 'block';
+        eyeIcon.style.display = 'block';
+        eyeOffIcon.style.display = 'none';
     });
-
-    function toggleEyeIcon() {
-        if (eyeIcon.style.display === 'none') {
-            eyeIcon.style.display = 'block';
-        } else {
-            eyeIcon.style.display = 'none';
-        }
-    }
 });
